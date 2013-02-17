e5d3b68f57e94d41b17436f05e8cb08fe69cbbad
John Kevin Basco <basco.johnkevin@gmail.com>
2013-02-16 19:07:46 -0800

    add contacts table seeder

---
+++ app/database/seeds/ContactsTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+class ContactsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('contacts')->delete();
+
+        DB::table('contacts')->insert(array( 
+ 												'name' =>  'kevin', 
+ 				   								'email' => 'basco.johnkevin@gmail.com'
+ 				   					  ));									 
+    }
+
+}
